050173958aa5d7c9019ea567a325b778df0a10cb
JudithBrener <judi.brener@gmail.com>
2020-10-10 14:57:23 +0300

    add Text-Based Approval Test

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,20 +1,21 @@
 package com.gildedrose;
 
+import com.google.common.io.Files;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+        Item[] items = getItems();
 
         GildedRose app = new GildedRose(items);
 
@@ -32,6 +33,40 @@ public class TexttestFixture {
             System.out.println();
             app.updateQuality();
         }
+    }
+
+    private static Item[] getItems() {
+        return new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+    }
+
+    @Test
+    public void updateQuality() throws IOException {
+        String expected = getExpected();
+        String actual = getActual();
+        assertEquals(expected, actual);
+    }
+
+    private String getExpected() throws IOException {
+        return Files.asCharSource(new File(
+                "C:\\Users\\Judi\\IdeaProjects\\GildedRose-Refactoring-Kata\\Java\\src\\test\\java\\com\\gildedrose\\goldenMaster"),
+                Charset.defaultCharset()).read();
+    }
+
+    private String getActual() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        main(new String[] {"30"});
+        return byteArrayOutputStream.toString();
     }
 
 }
